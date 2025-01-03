b41dd4cd7f880bce3fee1b0868968df231add412
Rafael Jordao <rafaeljordao.b@gmail.com>
2025-01-03 10:42:36 -0300

    feat: criando docker

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { routes } from "./routes";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "API Base",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(routes);
+
+app.listen({ port: 4000, host: "0.0.0.0" }).then(() => {
+  console.log("Servico rodando com sucesso na porta 4000");
+});
